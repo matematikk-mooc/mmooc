65375ae35ef4b000d7de0e2910ccf1cf5525cb57
etsikt <et@iktsenteret.no>
2017-12-08 13:55:19 +0100

    Removed a docker_run command.

--- mmdos.bat
+++ mmdos.bat
@@ -6,5 +6,4 @@ docker run --env-file=env -d -P --name=cache mmooc/cache
 docker run --rm --env-file=env -w /opt/canvas-lms --link=db:db --link=cache:cache mmooc/canvas bundle exec rake db:initial_setup >&2 2> init_schema.1.txt
 docker run --rm --env-file=env -w /opt/canvas-lms --link=db:db --link=cache:cache mmooc/canvas bundle exec rake db:initial_setup >&2 2> init_schema.2.txt
 docker run --env-file=env -d -P --volumes-from=web-data --link db:db --link cache:cache --name=web mmooc/canvas
-docker_run mmooc/haproxy haproxy "--link web:web1 -p 443:443 -p 80:80"
-docker run --env-file=env -d -P "--link --name=haproxy mmooc/haproxy
+docker run --env-file=env -d -P --link web:web1 -p 443:443 -p 80:80 --name=haproxy mmooc/haproxy
